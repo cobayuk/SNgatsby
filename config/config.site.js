0cc4c5e41125d5b624777290f5a584c8b7043305
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2020-02-23 13:19:10 +0700

    modify webpack merge and some config generator static site

---
+++ config/config.site.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const fs = require('fs');
+
+let ROOT = process.env.PWD;
+if (!ROOT) {
+  ROOT = process.cwd();
+}
+
+const config = {
+	// Your website's name, used for favicon meta tags
+	site_name: 'Sungnikah.com',
+	// Your website meta keywords user for meta keywords
+	site_keywords: 'jasa web invitation, wedding invitation, wedding invitation keren, undangan pernikahan digital',
+	// Your website's description, used for favicon meta tags
+	site_description: 'A modern boilerplate for static website development',
+	// Your website's URL, used for sitemap
+	site_url: 'http://sungnikah.netlify.com',
+	// Google Analytics tracking ID (leave blank to disable)
+	googleAnalyticsUA: '',
+	// The viewport meta tag added to your HTML page's <head> tag
+	viewport: 'width=device-width,initial-scale=1',
+	// Source file for favicon generation. 512x512px recommended.
+	favicon: path.join(ROOT, '/src/asssets/images/favicon.png'),
+	// Local development URL
+	dev_host: 'localhost',
+	// Local development port
+	port: process.env.PORT || 8081,
+	// Advanced configuration, edit with caution!
+	env: process.env.NODE_ENV,
+	root: ROOT,
+	paths: {
+		config: 'config',
+		src: 'src',
+		dist: 'dist',
+	},
+	package: JSON.parse(
+		fs.readFileSync(path.join(ROOT, '/package.json'), { encoding: 'utf-8' }),
+	),
+	// Webpack minification rules
+	webpackHtmlPlugins: {
+		minficationRules: false,
+	}
+};
+
+module.exports = config;
+  
---
+++ config/webpack/webpack.plugins.js
@@ -0,0 +1,126 @@
+const webpack = require('webpack');
+const cssnano = require('cssnano');
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+
+const WebpackBar = require('webpackbar');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const WebappWebpackPlugin = require('webapp-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const RobotstxtPlugin = require('robotstxt-webpack-plugin');
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+
+// config site 
+const config = require('../config.site');
+
+const hmr = new webpack.HotModuleReplacementPlugin();
+
+// Optimize CSS assets
+const optimizeCss = new OptimizeCssAssetsPlugin({
+	assetNameRegExp: /\.css$/g,
+	cssProcessor: cssnano,
+	cssProcessorPluginOptions: {
+	  preset: [
+		'default',
+      {
+        discardComments: {
+        removeAll: true,
+        },
+      },
+	  ],
+	},
+	canPrint: true,
+});
+
+// Generate HTML to dist folder (folder distribution)
+const paths = [];
+const generateHTMLPlugins = () => glob.sync('./src/**/*.html').map((dir) => {
+  const filename = path.basename(dir);
+
+  if (filename !== '404.html') {
+    paths.push(filename);
+  }
+
+  return new HTMLWebpackPlugin({
+    title: config.site_name,
+    filename,
+    template: path.join(config.root, config.paths.src, filename),
+    meta: {
+      viewport: config.viewport,
+      keywords: config.site_keywords,
+      description: config.site_description
+    },
+    minify: {
+      collapseWhitespace: false,
+      removeComments: true,
+      removeRedundantAttributes: false,
+      removeScriptTypeAttributes: false,
+      removeStyleLinkTypeAttributes: false,
+      useShortDoctype: false
+    },
+    hash: true
+  });
+});
+
+// Webpack bar
+const webpackBar = new WebpackBar({
+  color: '#ff6469',
+});
+
+// Clean webpack
+const clean = new CleanWebpackPlugin();
+
+
+// Extract CSS
+const cssExtract = new MiniCssExtractPlugin({
+  filename: 'style.[contenthash].css',
+});
+
+// Sitemap
+const sitemap = new SitemapPlugin(config.site_url, paths, {
+	priority: 1.0,
+	lastmodrealtime: true,
+});
+
+// Generate robots.txt
+const robots = new RobotstxtPlugin({
+  sitemap: `${config.site_url}/sitemap.xml`,
+  host: config.site_url,
+});
+
+// Favicons
+const favicons = new WebappWebpackPlugin({
+  logo: config.favicon,
+  prefix: 'images/favicon',
+  favicons: {
+    appName: config.site_name,
+    appDescription: config.site_description,
+    developerName: null,
+    developerURL: null,
+    icons: {
+      android: true,
+      appleIcon: true,
+      appleStartup: false,
+      coast: false,
+      favicons: true,
+      firefox: false,
+      windows: false,
+      yandex: false,
+    },
+  },
+});
+
+module.exports = [
+  clean,
+  cssExtract,
+	...generateHTMLPlugins(),
+  fs.existsSync(config.favicon) && favicons,
+  config.env === 'production' && optimizeCss,
+  config.env === 'production' && robots,
+  config.env === 'production' && sitemap,
+  webpackBar,
+  config.env === 'development' && hmr,
+].filter(Boolean);
