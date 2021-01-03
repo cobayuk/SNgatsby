6229b063ee5d241b43d9bdbbcf921f01cd7f05ff
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2021-01-03 16:33:57 +0700

    init migrate to gatsby js

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SEO from "../components/seo";
+import Layout from "../components/layout";
+
+const AboutUs = () => (
+  <Layout>
+    <SEO title="ini halaman about " />
+    <h2>Ini halaman about us !</h2>
+  </Layout>
+)
+
+export default AboutUs
---
+++ src/pages/client/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SEO from "../../components/seo";
+import Layout from "../../components/layout";
+
+const AboutUs = () => (
+  <Layout>
+    <SEO title="sungnikah client " />
+    <h2>Ini halaman about us !</h2>
+  </Layout>
+)
+
+export default AboutUs
