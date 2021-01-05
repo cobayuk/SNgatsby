ec548f5bbdfe3479f8dee0b93717c47b492aee59
Arie Aditya <arie.aditya@pricebook.co.id>
2021-01-05 21:37:17 +0700

    Add maintenance page and assets

---
+++ src/pages/maintenance.js
@@ -0,0 +1,27 @@
+import React from "react";
+import SEO from "../components/seo";
+import * as Style from "../../src/styles/maintenanceStyled";
+import ConstructionImage from "../images/under-construction.gif";
+import Instagram from "../images/instagram.svg";
+import Facebook from "../images/facebook.svg";
+
+const Maintenance = () => (
+    <>
+      <SEO title="Sungnikah - Jasa Pembuatan Wedding Website" />
+      <Style.Container>
+        <Style.ImageWrapper src={ConstructionImage} alt="under-construction"/>
+        <Style.Title>Website under construction</Style.Title>
+        <Style.Description>Welcome to <b>Sungnikah.com</b>, we are currently working on this site. <br/>Meanwhile, you can get updates in our social media below!</Style.Description>
+        <Style.Footer>
+          <a href="https://www.instagram.com/sungnikah/">
+            <Style.InstagramIcon src={Instagram} alt="instagram"/>
+          </a>
+          <a href="https://www.facebook.com/sungnikah/">
+            <Style.FacebookIcon src={Facebook} alt="facebook"/>
+          </a>
+        </Style.Footer>
+      </Style.Container>
+    </>
+)
+
+export default Maintenance
---
+++ src/styles/maintenanceStyled.js
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  text-align: center;
+  background-color: #4c53dc;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+`;
+
+export const ImageWrapper = styled.img`
+  text-align: center;
+  object-fit: cover;
+  width: 440px;
+  height: 440px;
+  margin-top: 20px;
+`;
+
+export const Title = styled.h1`
+  color: #ffffff;
+`;
+
+export const Description = styled.p`
+  color: #ffffff;
+  font-size: 12px;
+  line-height: 18px;
+  padding: 0px 30px;
+`;
+
+export const Footer = styled.div`
+  margin-top: 50px;
+`;
+
+export const InstagramIcon = styled.img`
+  width: 30px;
+  height: 30px;
+  margin-right: 30px;
+`;
+
+export const FacebookIcon = styled.img`
+  width: 30px;
+  height: 30px;
+`;
