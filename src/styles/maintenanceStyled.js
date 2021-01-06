921c6ea8659afdf1c88688025b25d033280bd091
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2021-01-06 07:08:28 +0700

    exported function global style

--- src/styles/maintenanceStyled.js
+++ src/styles/maintenanceStyled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import { createGlobalStyle } from 'styled-components';
 
-const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
     padding: 0;
