8f1aae56c7b054932bc3a51b99ffb609159f8c3b
Arie Aditya Nugraha <34788591+arieaditya@users.noreply.github.com>
2021-04-01 11:29:49 +0700

    Epic sungnikah (#20)
    * epic-sungnikah update readme
    
    * feat: add initial file for theme 2
    
    Co-authored-by: Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>

---
+++ src/pages/theme/secondStyle.js
@@ -0,0 +1,25 @@
+/*
+| ===========================================================
+|    This file contains all styles for Themes 2.
+| ===========================================================
+|
+| Author  : Sungnikah Corporation's.
+| Date    : 1 Apr 2021
+| Develop : Dimas Prasetyo, Arie Aditya Nugraha (Frontend Dev).
+*/
+
+import styled, { keyframes } from 'styled-components';
+import { Box, Typography, Button } from '@material-ui/core';
+// Fonts
+import "@fontsource/overlock";
+import "@fontsource/oxygen";
+import "@fontsource/satisfy";
+import "@fontsource/qwigley";
+import "@fontsource/dm-serif-display";
+
+export const CountDown = styled(Typography)`
+  &.MuiTypography-root {
+    font-size: 20px;
+    line-height: 25px;
+  }
+`;
