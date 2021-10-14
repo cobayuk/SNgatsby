dde32f99cdfcd6380bbd739af3940cf6fc70d96c
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2021-10-14 09:46:57 +0700

    EPIC-1421 add dotenv configuration and modifying gitignore

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 
 module.exports = {
   siteMetadata: {
@@ -74,6 +77,12 @@ module.exports = {
           "js"
         ]
       }
-    }
+    },
+    {
+      resolve: `gatsby-source-custom`,
+      options: {
+        apiKey: process.env.API_KEY,
+      },
+    },
   ],
 }
