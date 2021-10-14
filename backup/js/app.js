0964c6948462db92117481cd1b6a5c37a542f5c9
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2021-10-14 22:00:24 +0700

    DEV-1421 remove backup folder landing page old sungnikah

--- backup/js/app.js
+++
@@ -1,8 +0,0 @@
-// vendor js
-require('../js/jquery.js');
-// require('../js/jquery.easing.min.js');
-
-// function utils
-require('../js/functions.js');
-require('../js/updates.js');
-require('../js/owl.carousel.min.js');
