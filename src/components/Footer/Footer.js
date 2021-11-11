2810c2794e514a573f4471df2eabbb68b3d74423
Dimas Prasetyo <prasetyo.dimas@pricebook.co.id>
2021-11-12 00:52:21 +0700

    DEV-1221 add reach router and add some link at brand logo

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,16 +3,13 @@ import PropTypes from "prop-types";
 import {
   Grid,
   Link,
-  Avatar,
+  // Avatar,
   Container,
-  Typography,
+  // Typography,
   CssBaseline
 }
 from '@material-ui/core';
-import Ovo from '@assets/logo/digital-payment/ovo.svg';
-import Dana from '@assets/logo/digital-payment/dana.svg';
-import LinkAja from '@assets/logo/digital-payment/link-aja.svg';
-import Gopay from '@assets/logo/digital-payment/gopay.svg';
+// import Gopay from '@assets/logo/digital-payment/gopay.svg';
 import * as Style from '@components/Footer/FooterStyled';
 
 const Copyright = ({ titles }) => {
@@ -27,92 +24,14 @@ const Copyright = ({ titles }) => {
   );
 }
 
-const Footer = ({ siteTitle }) => {
+const Footer = () => {
   return (
     <Style.MainFooter>
       <CssBaseline />
       <Container fixed>
         <Grid container>
           <Grid item lg={3}>
-            <Style.HeadingWeLoveBuild variant="body1">
-              We Love To Build Right
-            </Style.HeadingWeLoveBuild>
-            <Style.HeadingTaglineBrand variant="caption">
-              Sungnikah.com cara pandai dan hemat membuat undangan digital.
-            </Style.HeadingTaglineBrand>
-            <Copyright siteTitle={siteTitle} />
-          </Grid>
-          <Style.ContainerDigitalPayment item lg={3}>
-            <Style.HeadingPaymentDigital variant="body1">
-              Pembayaran Digital
-            </Style.HeadingPaymentDigital>
-            <Grid container>
-              <Style.GridCenter item lg={3}>
-                <Style.WrapperInline>
-                  <img src={Ovo} alt="payment-ovo" />
-                  <Typography>OVO</Typography>
-                </Style.WrapperInline>
-              </Style.GridCenter>
-              <Style.GridCenter item lg={3}>
-                <Style.WrapperInline>
-                  <img src={Dana} alt="payment-dana" />
-                  <Typography>DANA</Typography>
-                </Style.WrapperInline>
-              </Style.GridCenter>
-              <Style.GridCenter item lg={3}>
-                <Style.WrapperInline>
-                  <img src={LinkAja} alt="payment-dana" />
-                  <Typography>LINK AJA</Typography>
-                </Style.WrapperInline>
-              </Style.GridCenter>
-              <Style.GridCenter item lg={3}>
-                <Style.WrapperInline>
-                  <img src={Gopay} alt="payment-dana" />
-                  <Typography>GOPAY</Typography>
-                </Style.WrapperInline>
-              </Style.GridCenter>
-            </Grid>
-          </Style.ContainerDigitalPayment>
-          <Style.ContainerDigitalConventional item lg={3}>
-            <Style.HeadingPaymentConvention variant="body1">
-              Pembayaran Konvensional
-            </Style.HeadingPaymentConvention>
-            <Grid container>
-              {/* <Grid item lg={4}>
-                <Style.WrapperInline>
-                  <Avatar>M</Avatar>
-                  <Typography>Mandiri</Typography>
-                </Style.WrapperInline>
-              </Grid>
-              <Grid item lg={4}>
-                <Style.WrapperInline>
-                  <Avatar>B</Avatar>
-                  <Typography>BNI</Typography>
-                </Style.WrapperInline>
-              </Grid>
-              <Grid item lg={4}>
-                <Style.WrapperInline>
-                  <Avatar>BC</Avatar>
-                  <Typography>BCA</Typography>
-                </Style.WrapperInline>
-              </Grid> */}
-            </Grid>
-          </Style.ContainerDigitalConventional>
-          <Grid item lg={3}>
-            <Style.WrapperMediaSocial>
-              <Style.BoxMediaSocial>
-                <Avatar>M</Avatar>
-              </Style.BoxMediaSocial>
-              <Style.BoxMediaSocial>
-                <Avatar>M</Avatar>
-              </Style.BoxMediaSocial>
-              <Style.BoxMediaSocial>
-                <Avatar>M</Avatar>
-              </Style.BoxMediaSocial>
-              <Style.BoxMediaSocial>
-                <Avatar>M</Avatar>
-              </Style.BoxMediaSocial>
-            </Style.WrapperMediaSocial>
+
           </Grid>
         </Grid>
       </Container>
