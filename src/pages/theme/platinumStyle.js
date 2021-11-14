b49ecaf8907f0ee31916ea14e7234a6ca041c58f
arieaditya <arie.aditya@pricebook.co.id>
2021-11-14 12:12:22 +0700

    Add responsive style for header platinum

--- src/pages/theme/platinumStyle.js
+++ src/pages/theme/platinumStyle.js
@@ -9,7 +9,7 @@
 */
 
 import styled from 'styled-components';
-import { Box, Typography, Button } from '@material-ui/core';
+import { Box, Grid, Typography, Button } from '@material-ui/core';
 import { breakpoints } from '@components/media';
 import AssetHeader from "@assets/photos/asset-3.jpeg";
 // Fonts
@@ -20,6 +20,9 @@ import "@fontsource/dancing-script";
 export const WrapperInvitation = styled(Box)`
     top: 160px;
     position: relative;
+    @media only screen and (min-width: 768px) and (max-width: 992px) {
+        top: 120px;
+    }
 `;
 
 export const Invite = styled.p`
@@ -28,6 +31,10 @@ export const Invite = styled.p`
     font-weight: normal;
     font-size: 24px;
     line-height: 45px;
+    @media (max-width: 600px) {
+        font-size: 14px;
+        line-height: 15px;
+    }
     color: white;
 `;
 
@@ -39,6 +46,10 @@ export const Title = styled.p`
     line-height: 37px;
     letter-spacing: 0.3em;
     color: white;
+    @media (max-width: 600px) {
+        font-size: 16px;
+        line-height: 15px;
+    }
 `;
 
 export const Groom = styled.p`
@@ -47,6 +58,17 @@ export const Groom = styled.p`
     font-weight: normal;
     font-size: 96px;
     line-height: 24px;
+    @media (max-width: 600px) {
+        font-size: 54px;
+        line-height: 12px;
+        text-align: center;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 992px) {
+        font-size: 80px;
+        line-height: 24px;
+        text-align: center;
+        margin-top: 50px;
+    }
     color: white;
     text-align: right;
 `;
@@ -57,6 +79,17 @@ export const Ampersand = styled.p`
     font-weight: normal;
     font-size: 96px;
     line-height: 24px;
+    @media (max-width: 600px) {
+        font-size: 36px;
+        line-height: 36px;
+        text-align: center;
+        margin: -20px 0px;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 992px) {
+        font-size: 80px;
+        line-height: 24px;
+        margin: -20px 0px;
+    }
     color: white;
 `;
 
@@ -66,6 +99,16 @@ export const Bride = styled.p`
     font-weight: normal;
     font-size: 96px;
     line-height: 24px;
+    @media (max-width: 600px) {
+        font-size: 54px;
+        line-height: 12px;
+        text-align: center;
+    }
+    @media only screen and (min-width: 768px) and (max-width: 992px) {
+        font-size: 80px;
+        line-height: 24px;
+        text-align: center;
+    }
     color: white;
     text-align: left;
 `;
@@ -76,6 +119,10 @@ export const CountDown = styled.p`
     font-weight: normal;
     font-size: 24px;
     line-height: 24px;
+    @media (max-width: 600px) {
+        font-size: 12px;
+        line-height: 17px;
+    }
     color: white;
 `;
 
@@ -87,4 +134,10 @@ export const Header = styled(Box)`
     position: relative;
     background-position: 50%;
     text-align: center;
+`;
+
+export const ResponsiveGrid = styled(Grid)`
+    @media (max-width: 600px) {
+        padding: 0 !important;
+    }
 `;
