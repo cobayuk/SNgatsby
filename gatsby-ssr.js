73ec4ee342f8c1bdb7cd1182f129ef8594a6ca04
Dimas Prasetyo <dimas@sahabatbisnis.co.id>
2021-12-30 09:17:36 +0700

    rollback configuration schema api

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,22 +1,22 @@
-import React from "react";
-import Layout from "./src/components/layout";
+// import React from "react";
+// import Layout from "./src/components/layout";
 
-export const wrapRootElement = ({ element, props }) => {
-  return (
-    <>
-      <Layout {...props}>
-        {element}
-      </Layout>
-    </>
-  );
-}
+// export const wrapRootElement = ({ element, props }) => {
+//   return (
+//     <>
+//       <Layout {...props}>
+//         {element}
+//       </Layout>
+//     </>
+//   );
+// }
 
-export const wrapPageElement = ({ element, props }) => {
-  return (
-    <>
-      <Layout {...props}>
-        {element}
-      </Layout>
-    </>
-  );
-}
+// export const wrapPageElement = ({ element, props }) => {
+//   return (
+//     <>
+//       <Layout {...props}>
+//         {element}
+//       </Layout>
+//     </>
+//   );
+// }
