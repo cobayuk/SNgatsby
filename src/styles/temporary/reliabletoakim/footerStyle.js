4e96ddea6c0e61bb0b5c9ec0b134d123a38869b1
arieaditya <arieadityan@gmail.com>
2022-11-22 23:35:42 +0700

    feat(styles): add styles for components

---
+++ src/styles/temporary/reliabletoakim/footerStyle.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+import "@fontsource/oxygen";
+import "@fontsource/open-sans/";
+import AssetHeader from "@assets/photos/akimaul/akim-landscape-2.jpg";
+
+export const Footer = styled.div`
+    width: 100vw;
+    height: 140px;
+    background:
+        linear-gradient(
+            rgba(0, 0, 0, 0.2),
+            rgba(0, 0, 0, 0.9)
+        ),
+        url(${AssetHeader});
+    }
+    background-size: cover;
+    position: relative;
+    background-position: 50% 25%;
+    margin-top: -40px;
+    @media (min-width: 768px) {
+        background-position: 50% 40%;
+        margin-top: -30px;
+    }
+    text-align: center;
+    z-index: 99;
+`;
+
+export const Hashtag = styled.span`
+    font-family: "Open Sans";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 18px;
+    line-height: 25px;
+    text-align: center;
+    color: #FFFFFF;
+    display: inline-block;
+    padding-top: 3em;
+}
+`;
+
+export const WrapperWatermark = styled.div`
+    font-family: Oxygen;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 17px;
+    text-align: center;
+    display: block;
+    margin: 16px 0px;
+    a {
+        text-decoration: none;
+        color: #E2B62E;
+    }
+`;
---
+++ src/styles/temporary/reliabletoakim/introStyle.js
@@ -0,0 +1,114 @@
+/*
+| ===========================================================
+|    This file contains Intro styles for Themes Platinum.
+| ===========================================================
+|
+| Author  : Sungnikah Corporation's.
+| Date    : 14 Nov 2021
+| Develop : Arie Aditya Nugraha, Dimas Prasetyo (Frontend Dev).
+*/
+
+import styled from 'styled-components';
+import { Grid } from '@material-ui/core';
+// Fonts
+import "@fontsource/crimson-text";
+import "@fontsource/dancing-script";
+
+export const WrapperIntro = styled.div`
+    background-color: #F1EDE7;
+    padding-bottom: 32px;
+`;
+
+export const ContainerIntro = styled.div`
+    padding: 30px 100px 0px 100px;
+    @media (max-width: 600px) {
+        padding: 30px 50px 0px 50px;
+    }
+`;
+
+export const IntroText = styled.p`
+    font-family: Crimson Text;
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 15px;
+    text-align: center;
+    color: #58595B;
+    @media (max-width: 600px) {
+        font-size: 16px;
+        line-height: 20px;
+    }
+`;
+
+export const Ampersand = styled.p`
+    font-family: Dancing Script;
+    font-weight: normal;
+    font-size: 9em;
+    line-height: 24px;
+    text-align: center;
+    color: #B37C62;
+    display: block;
+    margin-top: 50px;
+    @media (max-width: 600px) {
+        font-size: 5em;
+        line-height: 24px;
+    }
+`;
+
+export const Parent = styled.p`
+    font-family: Crimson Text;
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 15px;
+    text-align: center;
+    color: #58595B;
+    margin-top : -30px;
+    @media (max-width: 600px) {
+        font-size: 16px;
+        line-height: 20px;
+    }
+`;
+
+export const GridCustom = styled(Grid)`
+    align-items: baseline;
+    margin-bottom: 40px;
+`;
+
+export const Poster = styled.img`
+    border-radius: 50%;
+    max-width: 100%;
+`;
+
+export const GroomWrapper = styled.div`
+    margin-top: -24px;
+    @media (max-width: 600px) {
+        padding: 0px 25px;
+    }
+`;
+
+export const BrideWrapper = styled.div`
+    @media (max-width: 600px) {
+        padding: 0px 25px;
+    }
+`;
+
+export const AvatarWrapper = styled.div`
+    text-align: center;
+    margin-top: 40px;
+    margin-bottom: -20px;
+`;
+
+export const PersonName = styled.p`
+    font-family: Crimson Text;
+    font-weight: 400;
+    text-align: center;
+    color: #B37C62;
+    margin: 40px 0px;
+    @media (min-width: 320px) {
+      font-size: 24px;
+      line-height: 16px;
+    }
+    @media (min-width: 600px) {
+        font-size: 24px;
+        line-height: 16px;
+    }
+`;
